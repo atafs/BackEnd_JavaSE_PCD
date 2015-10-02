294de2de20da29f444062da71c962d711455e1f0
atafs <atafs@iscte-iul.pt>
2015-10-02 20:28:20 +0100

    change design of calculator

--- LinguagemJAVA_super/src/control_3CD/Start_PCD_week01/_2_1_MaquinadeCalcular/Calculator.java
+++ LinguagemJAVA_super/src/control_3CD/Start_PCD_week01/_2_1_MaquinadeCalcular/Calculator.java
@@ -1,45 +1,112 @@
 package control_3CD.Start_PCD_week01._2_1_MaquinadeCalcular;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class Calculator extends JFrame {
+public class Calculator{
+
+	//ATTRIBUTES
+	private JFrame container;
+	private JTextField display;
+	private String displayContext = "";
 	
 	public Calculator() {
- // Create panel p1 for the buttons and set GridLayout
-		JPanel p1 =  new JPanel();
-		p1.setLayout( new GridLayout( 4 , 3 ));
+		container = new JFrame();
 		
+		// Create panel p1 for the buttons and set GridLayout
+		JPanel p1 = new JPanel();
+		p1.setLayout(new GridLayout(4, 4));
+
+		//HARD CODED SOLUTION
+		// ADD 7,8,9,+
+		p1.add(new JButton("7"));
+		p1.add(new JButton("8"));
+		p1.add(new JButton("9"));	
+		JButton add = new JButton("+");
+		p1.add(add);
+		add.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("+");
+				displayContext += "+";
+				display.setText(displayContext);
+			}
+		});
+		
+		// ADD 4,5,6,-
+		p1.add(new JButton("4"));
+		p1.add(new JButton("5"));
+		p1.add(new JButton("6"));
+		p1.add(new JButton("-"));
+		
+		// ADD 1,2,3,*
+		p1.add(new JButton("1"));
+		p1.add(new JButton("2"));
+		p1.add(new JButton("3"));
+		p1.add(new JButton("*"));
+		
+		// ADD 1,2,3,*
+		p1.add(new JButton("0"));
+		p1.add(new JButton("."));
+		p1.add(new JButton("^"));
+		p1.add(new JButton("/"));
+		
+
+		// Create panel p2 to hold a text field and p1
+		JPanel p2 = new JPanel(new BorderLayout());
+		p2.setLayout(new GridLayout(2, 1));
+
+		//SEPARETE BUTTONS
+		JButton clean = new JButton("C");
+
+		p2.add(clean);
+		add.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				displayContext = "0";
+				display.setText(displayContext);
+			}
+		});
+		p2.add(new JButton("="));
+		
+		//DISPLAY
+		display = new JTextField("write values");
+		display.setBackground(Color.GREEN);
+
+		// Add contents to the frame
+		container.setLayout(new BorderLayout());
+		container.add(p1, BorderLayout.WEST);
+		container.add(p2, BorderLayout.EAST);
+		container.add(display, BorderLayout.CENTER);
+
+	}
 	
- // Add buttons to the panel
- for ( int i =  1 ; i <=  9 ; i++) {
-	 p1.add( new JButton( "" + i));
-      }
+	/** */
+	public void init(JFrame frame) {
+		frame.setTitle("CALCULATOR ISCTE IUL");
+		frame.setSize(800, 300);
+		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-      p1.add( new JButton( "" + 0 ));
-      p1.add( new JButton( "Start" ));
-      p1.add( new JButton( "Stop" ));
+	}
+	
+	/** */
+	public void execute(Frame frame) {
+		frame.setVisible(true);
 
- // Create panel p2 to hold a text field and p1
-      JPanel p2 =  new JPanel( new BorderLayout());
-
-      p2.add( new JTextField( "Time to be displayed here" ),
-        BorderLayout.NORTH);
-      p2.add(p1, BorderLayout.CENTER);
-
- // Add contents to the frame
-      add(p2, BorderLayout.EAST); 
-      add( new JButton( "Food to be placed here" ),
-        BorderLayout.CENTER);
-    }
+	}
 
 	/** Main method */
 	public static void main(String[] args) {
-		Calculator frame = new Calculator();
-		frame.setTitle("The Front View of a Microwave Oven");
-		frame.setSize(400, 250);
-		frame.setLocationRelativeTo(null); // Center the frame
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		Calculator gui = new Calculator();
+		JFrame frame = gui.container;
+		gui.init(frame);
+		gui.execute(frame);
+		
 	}
 }
