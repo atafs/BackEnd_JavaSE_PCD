f8beb8ece34ed16a851f0f86e4228e0bc1324a38
atafs <atafs@iscte-iul.pt>
2015-10-23 00:17:29 +0100

    update

---
+++ LinguagemJAVA_super/src/control_3CD/Start_PCD_week05/_5_0/DataPackage.java
@@ -0,0 +1,14 @@
+package control_3CD.Start_PCD_week05._5_0;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class DataPackage implements Serializable{
+	
+	/*attributes*/
+	public float x = 0.0f;
+	public float y = 0.0f;
+	
+	public String username = "";
+
+}
